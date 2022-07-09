9bf29eb2bf8f442e3954ac08405793bc5bafd43c
izik4004 <izik4004@gmail.com>
2022-07-09 15:17:14 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero/Hero';
 import Favorite from './components/Favourite/Favorite';
