677fd1e84ed5237be9217e240a706e080e680ac4
izik4004 <izik4004@gmail.com>
2022-08-28 02:08:16 +0100

    Updates

---
+++ precache-manifest.29d6ec19daa0f653b28012c44b79f914.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e55388f810b7a2e627dc",
+    "url": "/restaurant_landing_page/static/css/main.22324b8e.chunk.css"
+  },
+  {
+    "revision": "e55388f810b7a2e627dc",
+    "url": "/restaurant_landing_page/static/js/main.e55388f8.chunk.js"
+  },
+  {
+    "revision": "a0985098e435df5dd837",
+    "url": "/restaurant_landing_page/static/js/1.a0985098.chunk.js"
+  },
+  {
+    "revision": "b5c00471ec69b703907c",
+    "url": "/restaurant_landing_page/static/js/runtime~main.b5c00471.js"
+  },
+  {
+    "revision": "a90017b675eaac9379213cc951f28665",
+    "url": "/restaurant_landing_page/static/media/b3.a90017b6.jpg"
+  },
+  {
+    "revision": "386dde63b741b30a7342f1621637a5fb",
+    "url": "/restaurant_landing_page/static/media/b1.386dde63.jpg"
+  },
+  {
+    "revision": "d660d8f5077d60dc117a9d2eecd582ec",
+    "url": "/restaurant_landing_page/static/media/b2.d660d8f5.jpg"
+  },
+  {
+    "revision": "459d7b5435cfd6c809e7df03398505f5",
+    "url": "/restaurant_landing_page/static/media/b4.459d7b54.jpg"
+  },
+  {
+    "revision": "d72417655dce49f5e73edcc075240581",
+    "url": "/restaurant_landing_page/static/media/b5.d7241765.jpg"
+  },
+  {
+    "revision": "c64ab4507f3346a0d8fff3fc9e608808",
+    "url": "/restaurant_landing_page/static/media/b7.c64ab450.jpg"
+  },
+  {
+    "revision": "a49b2ed8e7a7cfaa2455927242491444",
+    "url": "/restaurant_landing_page/static/media/b9.a49b2ed8.jpg"
+  },
+  {
+    "revision": "6e290eaf0cb881c502909c6d3dcdac4b",
+    "url": "/restaurant_landing_page/static/media/chef1.6e290eaf.jpg"
+  },
+  {
+    "revision": "d6eb0b41e141767db3e74a5ef10e4711",
+    "url": "/restaurant_landing_page/static/media/chef2.d6eb0b41.jpg"
+  },
+  {
+    "revision": "9d7fe2c8705d09b4870f92e8ff95134a",
+    "url": "/restaurant_landing_page/static/media/burger.9d7fe2c8.jpg"
+  },
+  {
+    "revision": "b8968d85c583cb31aedd6db73dd098c5",
+    "url": "/restaurant_landing_page/static/media/b10.b8968d85.jpg"
+  },
+  {
+    "revision": "59825867a19de11861d385bcb53491c8",
+    "url": "/restaurant_landing_page/static/media/kitchen.59825867.jpg"
+  },
+  {
+    "revision": "725ce8ef3e92ed9edbb93181d6710d82",
+    "url": "/restaurant_landing_page/index.html"
+  }
+];
